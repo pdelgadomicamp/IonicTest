287866b1d509d849928465638a010f71291e4b6b
pdelgadomicamp <pdelgado@micamp.com>
2023-03-16 11:17:58 -0700

    UPDATE
    new changes for new build

--- android/app/src/main/java/com/pablodelgado/ionictest/MainActivity.java
+++ android/app/src/main/java/com/pablodelgado/ionictest/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.pablodelgado.ionictest;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'testApp',
+  appId: 'com.pablodelgado.ionictest',
+  appName: 'ionicTest',
   webDir: 'www',
   bundledWebRuntime: false
 };
